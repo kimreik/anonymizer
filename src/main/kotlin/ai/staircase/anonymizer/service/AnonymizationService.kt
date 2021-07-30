ace51fdfebd46adb08bcbab88516dc335fce6bfe
Ilya Zeliankevich <kimreiks2lan@gmail.com>
2021-07-30 12:52:50 +0300

    javadoc

--- src/main/kotlin/ai/staircase/anonymizer/service/AnonymizationService.kt
+++ src/main/kotlin/ai/staircase/anonymizer/service/AnonymizationService.kt
@@ -3,6 +3,14 @@ package ai.staircase.anonymizer.service
 import ai.staircase.anonymizer.service.replacer.Replacer
 import org.springframework.stereotype.Service
 
+/**
+ * A simple implementation based on regex replacers.
+ *
+ * Regexes are relatively slow and every replacer recreates the string.
+ * The better way would be to implement a tokenizer,
+ * process the tokens (potentially in parallel) and rebuild the string from it.
+ * But it would take much more time.
+ */
 @Service
 class AnonymizationService(val replacers: List<Replacer>) {
 
